b22db0f8d32002f2283cc54038654d6534a7b380
pcresswell <pcresswell@gmail.com>
2015-12-05 14:45:01 -0500

    Adding Core Test Project

---
+++ Beaker.Core.Test/Beaker.Core.Test/TestPatient.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using NUnit.Framework;
+using Beaker.Core;
+
+namespace Beaker.Core.Test
+{
+    [TestFixture]
+    public class TestPatient
+    {
+        [Test]
+        public void TestTruth()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [Test]
+        public void TestPatientHasAFirstAndLastName()
+        {
+            Patient patient = new Patient(firstName: "Peter", lastName: "Cresswell");
+            Assert.AreEqual("Peter", patient.FirstName);
+            Assert.AreEqual("Cresswell", patient.LastName);
+        }
+    }
+}
---
+++ Beaker.Core/Beaker.Core/Patient.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Beaker.Core
+{
+    public class Patient
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public Patient(string firstName, string lastName)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+        }
+    }
+}
