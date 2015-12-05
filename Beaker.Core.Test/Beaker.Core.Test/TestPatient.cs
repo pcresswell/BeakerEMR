d94491c1a200d4af4a2d7efee6448b4a81428708
pcresswell <pcresswell@gmail.com>
2015-12-05 16:21:35 -0500

    Close #1 - Patient now has name, prefix, suffix, gender and date of birth

--- Beaker.Core.Test/Beaker.Core.Test/TestPatient.cs
+++ Beaker.Core.Test/Beaker.Core.Test/TestPatient.cs
@@ -7,6 +7,17 @@ namespace Beaker.Core.Test
     [TestFixture]
     public class TestPatient
     {
+        [SetUp]
+        public void Setup()
+        {
+            patient = new Patient {
+                FirstName = "Peter",
+                LastName = "Cresswell"
+            };    
+        }
+
+        private Patient patient;
+
         [Test]
         public void TestTruth()
         {
@@ -14,11 +25,61 @@ namespace Beaker.Core.Test
         }
 
         [Test]
-        public void TestPatientHasAFirstAndLastName()
+        public void PatientHasAFirstAndLastName()
         {
-            Patient patient = new Patient(firstName: "Peter", lastName: "Cresswell");
-            Assert.AreEqual("Peter", patient.FirstName);
-            Assert.AreEqual("Cresswell", patient.LastName);
+            Assert.AreEqual("Peter", this.patient.FirstName);
+            Assert.AreEqual("Cresswell", this.patient.LastName);
         }
+
+        [Test]
+        public void PatientHasAMiddleName()
+        {
+            this.patient.MiddleName = "Graydon";
+            Assert.AreEqual("Graydon", this.patient.MiddleName);
+        }
+
+        [Test]
+        public void PatientHasAPrefixAndSuffix()
+        {
+            this.patient.Prefix = "Mr.";
+            this.patient.Suffix = "3rd";
+
+            Assert.AreEqual("Mr.", this.patient.Prefix);
+            Assert.AreEqual("3rd", this.patient.Suffix);
+        }
+       
+        [Test]
+        public void PatientMayHaveAGender()
+        {
+            this.patient.Gender = Gender.Male;
+            Assert.AreEqual(this.patient.Gender, Gender.Male);
+            Assert.AreEqual(this.patient.Gender, new Gender("Male"));
+        }
+
+        [Test]
+        public void PatientByDefaultHasUnknownGender()
+        {
+            Assert.AreEqual(this.patient.Gender, Gender.Unknown);
+        }
+
+        [Test]
+        public void PatientNameIsEmptyByDefault()
+        {
+           this. patient = new Patient();
+            Assert.AreEqual(this.patient.FirstName, string.Empty);
+            Assert.AreEqual(this.patient.LastName, string.Empty);
+            Assert.AreEqual(this.patient.MiddleName, string.Empty);
+            Assert.AreEqual(this.patient.Prefix, string.Empty);
+            Assert.AreEqual(this.patient.Suffix, string.Empty);
+        }
+
+        [Test]
+        public void PatientByDefaultIsBornOnJan11899()
+        {
+            Assert.AreEqual(this.patient.DateOfBirth, new DateTime(1899, 1, 1));
+            this.patient.DateOfBirth = new DateTime(1978, 3, 31);
+            Assert.AreEqual(new DateTime(1978, 3, 31), this.patient.DateOfBirth);
+        }
+
     }
 }
---
+++ Beaker.Core/Beaker.Core/Gender.cs
@@ -0,0 +1,80 @@
+﻿/*  
+
+The MIT License (MIT)
+
+Copyright (c) 2015 Peter Cresswell (pcresswell@gmail.com)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*/
+
+namespace Beaker.Core
+{
+
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Gender class. Represents a person's gender.
+    /// </summary>
+    public class Gender
+    {
+        /// <summary>
+        /// The Male gender
+        /// </summary>
+        public static readonly Gender Male = new Gender("Male");
+
+        /// <summary>
+        /// The Female gender
+        /// </summary>
+        public static readonly Gender Female = new Gender("Female");
+
+        /// <summary>
+        /// Default gender. Unknown gender.
+        /// </summary>
+        public static readonly Gender Unknown = new Gender("Unknown");
+
+        public string Name { get; private set; }
+
+        public Gender(string name)
+        {
+            this.Name = name;
+        }
+
+        public override bool Equals(object obj)
+        {
+            Gender otherGender = obj as Gender;
+            if (otherGender == null)
+            {
+                return false;
+            }
+
+            return this.Name.Equals(otherGender.Name);
+        }
+
+        public override int GetHashCode()
+        {
+            return this.Name.GetHashCode();
+        }
+
+    }
+}
