de201505b5a5f0bb7d4fc1bc57d31da75ab64cc0
pcresswell <pcresswell@gmail.com>
2015-12-31 11:29:58 -0500

    Deleting memory repository
    No longer needed. Use sqlite in memory instead.

--- Beaker.Extensions/Properties/AssemblyInfo.cs
+++ Beaker.Extensions/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Beaker.Repository.Memory")]
+[assembly: AssemblyTitle("Beaker.Extensions")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Beaker.Repository.Memory")]
+[assembly: AssemblyProduct("Beaker.Extensions")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
